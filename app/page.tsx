60ab687697ce57ef3e5af2a32c9712ef807bb8a5
tristan-silvijn <ensingsil@gmail.com>
2025-05-05 13:59:39 +0200

    guessmynumber

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <script type="text/javascript" src="/guess-number.js" />
       <h1>Cheese of sil</h1>
       <Image 
       src="/goatCheeseWheel.webp"
       width={175}
-      height={225}
+      height={175}
       alt="ultimate cheese"></Image>
     </>
   );
---
+++ public/guess-number.js
@@ -0,0 +1,25 @@
+console.log("WHAAAT!")
+
+let lastguess = 0
+let biggerlimit = 100
+let smallerlimit = 1
+
+function guessmynumber(){ 
+    let additionResult = (smallerlimit + biggerlimit)
+    let divisionResult = additionResult / 2
+    let result = Math.round( divisionResult )
+    lastguess = result
+    return result
+}
+
+function smaller(){
+    biggerlimit = lastguess
+    let result = guessmynumber()
+    return result
+}
+
+function bigger (){
+    smallerlimit = lastguess
+    let result = guessmynumber()    
+    return result
+}
