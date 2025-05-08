4afd51cae31751ddea1a30fd24911085a34de944
bannanabommer <ensingsil@gmail.com>
2025-05-08 10:51:11 +0200

    added a links

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,18 @@ import RotatingCheeseWheel from "./RoatingCheeseWheel";
 export default function Home() {
   return (
     <>
-      <script type="text/javascript" src="/guess-number.js" />
-      <h1>Cheese of sil</h1>
-      <RotatingCheeseWheel imageUrl="/GoatCheeseWheel.webp" />
-      <Link href="/guess-my-number" >Guess My Number</Link>
+      <header>
+        <h1>Cheese of sil</h1>
+      </header>
+      <main className="grid grid-cols-5">
+        <div>
+          <Link className="block" href="/guess-my-number" >Guess My Number</Link>
+          <Link className="block" href="/wizards-game" >Wizard Game</Link>
+        </div>  
+        <div className="col-span-4">
+          <RotatingCheeseWheel imageUrl="/GoatCheeseWheel.webp" />
+        </div>
+      </main>
     </>
   );
 }
